4064e69dc7f5124766ec9f05b4737ea9e99bb89d
Luka Brkovic <coding@lbrkovic.com>
2023-05-12 08:13:31 +0200

    styling

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -18,36 +18,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const redirectToWatch = useCallback(() => router.push(`/watch/${data.id}`), [router, data.id]);
 
   return (
-    <div className="group bg-zinc-900 col-span relative h-[12vw]">
-      <img onClick={redirectToWatch} src={data.thumbnailUrl} alt="Movie" draggable={false} className="
-        cursor-pointer
-        object-cover
-        transition
-        duration
-        shadow-xl
-        rounded-md
-        group-hover:opacity-90
-        sm:group-hover:opacity-0
-        delay-300
-        w-full
-        h-[12vw]
+    <div className="group bg-neutral-50 col-span relative h-[12vw]">
+      <img onClick={redirectToWatch} src={data.thumbnailUrl} alt="Movie" draggable={false} className=" cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]
       " />
-      <div className="
-        opacity-0
-        absolute
-        top-0
-        transition
-        duration-200
-        z-10
-        invisible
-        sm:visible
-        delay-300
-        w-full
-        scale-0
-        group-hover:scale-110
-        group-hover:-translate-y-[6vw]
-        group-hover:translate-x-[2vw]
-        group-hover:opacity-100
+      <div className=" opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100
       ">
         <img onClick={redirectToWatch} src={data.thumbnailUrl} alt="Movie" draggable={false} className="
           cursor-pointer
@@ -59,16 +33,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           w-full
           h-[12vw]
         " />
-        <div className="
-          z-10
-          bg-zinc-800
-          p-2
-          lg:p-4
-          absolute
-          w-full
-          transition
-          shadow-md
-          rounded-b-md
+        <div className=" z-10 bg-neutral-400 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md
           ">
           <div className="flex flex-row items-center gap-3">
             <div onClick={redirectToWatch} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
