95e3f657cb083fcb4b0d9417f2237f733346b9c4
Luka Brkovic <coding@lbrkovic.com>
2023-05-12 08:11:12 +0200

    movie card and import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Hero from "@/components/Hero";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 import { type NextPageContext } from "next"
 import { getSession } from "next-auth/react"
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+
 export async function getServerSideProps(context: NextPageContext) {
 const session = await getSession(context);
 
@@ -19,11 +23,18 @@ return {
 }
 
 const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
   return (
 <> 
 <Navbar />
 <Hero />
-
+<div className="pb-40">
+<div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+</div>
+</div>
 </>
 )
 }
